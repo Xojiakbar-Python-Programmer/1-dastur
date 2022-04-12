0ab23c593d87b32c2c28458617d7bbf3bc75e4f7
Xojiakbar Tirkashev Dilmurodjon oli <100460105+Xojiakbar-Python-Programmer@users.noreply.github.com>
2022-04-12 08:58:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas. ' )
+# 5 ning darajasini toping
+print(5**2)
+# 22 ni 4 ga bo`lgandagi qoldiq
+print(22%4)
+# Tomonlari 125 ga teng bo`lgan kvadrating yuzi va perimetri
+print("Kvadrat yuzi S =",125**2,"Kvadrat perimetri P =",125*4)
+# Tomonlari 12 ga teng bo`lgan doiraning yuzi 
+print("Doira radiusi r=",12/2,"Doira yuzi",2*3.14*6**2)
+#Uchburchak gipotenuzasi
+print("Gipotenuza C =",(6**2+7**2)**(1/2))
